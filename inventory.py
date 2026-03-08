4786e5860bb0fccff53bdc1d576c89463d89af09
karnikjain <karnikjain8@gmail.com>
2026-03-09 01:11:35 +0530

    new functions

--- inventory.py
+++ inventory.py
@@ -10,3 +10,33 @@ def add_item(code):
         print (inventory[code]["quantity"])
         with open("inventory.json","w") as f:
             json.dump(inventory,f,indent=4)
+
+    if code not in inventory:
+        inventory[code] = {
+            "name" : input("What is the name?"),
+            "quantity" : int(input("What is the quantity?")),
+            "price" : int(input("What is the price?")),
+        }
+
+
+def remove_items(code):
+    if code in inventory:
+        inventory[code]["quantity"] = int(inventory[code]["quantity"]) - 1
+        print(inventory[code]["quantity"])
+        with open("inventory.json", "w") as f:
+            json.dump(inventory, f, indent=4)
+
+    if code not in inventory:
+        print("No items found")
+
+
+
+
+
+def give_details(code):
+    if code in inventory:
+        print(inventory[code])
+
+
+    if code not in inventory:
+        print("No items found")
